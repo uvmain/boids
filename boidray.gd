c73a409327ff5e4d7436c2b350b40b4e96e4b01b
Ian Baron <uv.fragm@gmail.com>
2022-12-31 14:08:24 +0000

    minor optimisation

--- boidray.gd
+++ boidray.gd
@@ -29,17 +29,20 @@ func _ready():
 
 
 func _process(delta):
-	check_for_offscreen()
 	boid_list.clear()
+	close_boids.clear()
+	
+	check_for_offscreen()
+	
 	for ray in rays:
 		var collider = ray.get_collider()
 		if collider:
 			boid_list.append(collider)
 	if boid_list:
 		var flock_data = flock_rules()
-		direction += flock_data.alignment * alignment_amount
-		direction += flock_data.cohesion * cohesion_amount
-		direction += flock_data.separation * separation_amount
+		direction += flock_data[0] * cohesion_amount
+		direction += flock_data[1] * alignment_amount
+		direction += flock_data[2] * separation_amount
 		
 	if main.follow_mouse:
 		var mouse_pos = ((get_viewport().get_mouse_position() - position)).normalized()
@@ -50,11 +53,11 @@ func _process(delta):
 	translate(velocity * delta * boid_speed)
 
 
-func flock_rules() -> Dictionary:
+func flock_rules() -> Array:
 	var cohesion = Vector2.ZERO
 	var alignment = Vector2.ZERO
 	var separation = Vector2.ZERO
-	close_boids.clear()
+	var boid_list_size = boid_list.size()
 	
 	for boid in boid_list:
 		cohesion += boid.position
@@ -63,19 +66,19 @@ func flock_rules() -> Dictionary:
 			close_boids.append(boid)
 			var difference = position - boid.position
 			separation += difference.normalized() / difference.length()
-	cohesion = (((cohesion / boid_list.size()) - position) - velocity).normalized()
-	alignment = (((alignment / boid_list.size()) - position) - velocity).normalized()
+	cohesion = (((cohesion / boid_list_size) - position) - velocity).normalized()
+	alignment = (((alignment / boid_list_size) - position) - velocity).normalized()
 	if close_boids:
 		separation = (((separation / close_boids.size()) - position) - velocity).normalized()
-	return { "cohesion": cohesion, "alignment": alignment, "separation": separation }
+	return [cohesion, alignment,separation]
 
 
 func check_for_offscreen():
 		if position.x < 0:
 			position.x = screensize.x
-		if position.x > screensize.x:
+		elif position.x > screensize.x:
 			position.x = 0
 		if position.y < 0:
 			position.y = screensize.y
-		if position.y > screensize.y:
+		elif position.y > screensize.y:
 			position.y = 0
